b73513b8572e50538c269435369be7c32283d382
Konstantinos Bairaktaris <kbairak@transifex.com>
2016-02-15 23:34:39 +0200

    Compile, wheeee!!!

--- openformats/formats/lxml_android.py
+++ openformats/formats/lxml_android.py
@@ -14,13 +14,15 @@ class LxmlAndroidHandler(Handler):
 
     def parse(self, content):
         # find starting tag
-        start = content.index('<resources')
+        resources_tag_position = content.index('<resources')
 
-        template = content[:start]
+        template = content[:resources_tag_position]
         self.starting_line_number = template.count('\n')
         stringset = []
 
-        self.root = etree.fromstring(content[start:].encode('UTF-8'))
+        self.root = etree.fromstring(
+            content[resources_tag_position:].encode('UTF-8')
+        )
 
         self.last_comment = ""
         self._order = itertools.count()
@@ -50,7 +52,8 @@ class LxmlAndroidHandler(Handler):
                     stringset.append(string)
                 self.last_comment = ""
 
-        return template + etree.tostring(self.root), stringset
+        template += etree.tostring(self.root)
+        return template, stringset
 
     def _handle_string_tag(self, element):
         try:
@@ -155,6 +158,8 @@ class LxmlAndroidHandler(Handler):
                             order=next(self._order),
                             developer_comment=self.last_comment)
 
+        # Now that we have the hash from the string, lets make another pass to
+        # replace the <item>s; we will only keep the rule=5 item
         for item in new_plurals_element:
             if self.get_rule_number(item.attrib['quantity']) == 5:
                 new_item = self._copy_element(item)
@@ -177,8 +182,103 @@ class LxmlAndroidHandler(Handler):
         end = len(string) - string[::-1].index('<') - 1
         return string[start:end]
 
+    def compile(self, template, stringset):
+        resources_tag_position = template.index("<resources")
+        compiled = template[:resources_tag_position]
+        self.root = etree.fromstring(template.encode('UTF-8'))
+
+        self._stringset = iter(stringset)
+
+        for element in self.root:
+            if element.tag == "string":
+                self._compile_string(element)
+            elif element.tag == "string-array":
+                self._compile_string_array(element)
+            elif element.tag == "plurals":
+                self._compile_plurals(element)
+
+        compiled += etree.tostring(self.root)
+        return compiled
+
+    def _compile_string(self, element):
+        try:
+            next_string = next(self._stringset)
+        except StopIteration:
+            next_string = None
+
+        if (next_string is not None and
+                next_string.template_replacement == element.text):
+            # Found one to replace
+            new_element = self._copy_element(element)
+            # Do this in order to preserve XML-characters in string; tag name
+            # is irrelevant
+            replacement_xml = etree.fromstring("<a>{}</a>".
+                                               format(next_string.string))
+            new_element.text = replacement_xml.text
+            for item in replacement_xml:
+                new_element.append(item)
+            self.root.replace(element, new_element)
+        else:
+            # Didn't find it, must remove it
+            self.root.remove(element)
+
+    def _compile_string_array(self, array_element):
+        new_array_element = deepcopy(array_element)
+
+        for item_element in new_array_element:
+            try:
+                next_string = next(self._stringset)
+            except StopIteration:
+                next_string is None
+
+            if (next_string is not None and
+                    next_string.template_replacement == item_element.text):
+                # Found one to replace
+                new_item_element = self._copy_element(item_element)
+                replacement_xml = etree.fromstring("<a>{}</a>".
+                                                   format(next_string.string))
+                new_item_element.text = replacement_xml.text
+                for item in replacement_xml:
+                    new_item_element.append(item)
+                new_array_element.replace(item_element, new_item_element)
+            else:
+                # Didn't find it, must remove it
+                new_array_element.remove(item_element)
+
+        if len(new_array_element) > 0:
+            self.root.replace(array_element, new_array_element)
+        else:
+            self.root.remove(array_element)
+
+    def _compile_plurals(self, plurals_element):
+        # We expect a single <item> tag here
+        assert len(plurals_element) == 1
+        item_element = plurals_element[0]
+        try:
+            next_string = next(self._stringset)
+        except StopIteration:
+            next_string = None
+        if (next_string is not None and
+                next_string.template_replacement == item_element.text):
+            # Found one to replace
+            new_plurals_element = self._copy_element(plurals_element)
+            for rule, string in sorted(next_string.string.items(),
+                                       key=lambda i: i[0]):
+                new_item_element = self._copy_element(item_element)
+                new_item_element.attrib['quantity'] =\
+                    self.get_rule_string(rule)
+                replacement_xml = etree.fromstring("<a>{}</a>".format(string))
+                new_item_element.text = replacement_xml.text
+                for item in replacement_xml:
+                    new_item_element.append(item)
+                new_plurals_element.append(new_item_element)
+            self.root.replace(plurals_element, new_plurals_element)
+        else:
+            self.root.remove(plurals_element)
+
     @staticmethod
     def _copy_element(element):
+        "Returns a copy of the 'element', stripping all its contents"
         new_element = deepcopy(element)
         for item in new_element:
             new_element.remove(item)
